815d04eeb998a8ea153d2ab0c97f978c858d832d
volcov <volcov.bruno@gmail.com>
2025-09-03 23:45:43 -0300

    chore: add docs

--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule LetItCrash.MixProject do
       deps: deps(),
       package: package(),
       source_url: @source_url,
-      description: description()
+      description: description(),
+      docs: docs()
     ]
   end
 
@@ -27,7 +28,8 @@ defmodule LetItCrash.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
@@ -46,6 +48,20 @@ defmodule LetItCrash.MixProject do
       licenses: ["MIT"],
       links: %{"GitHub" => @source_url},
       maintainers: ["volcov"]
+    ]
+  end
+
+  defp docs do
+    [
+      main: "LetItCrash",
+      name: "LetItCrash",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: ["README.md", "CHANGELOG.md"],
+      groups_for_extras: [
+        "Getting Started": ["README.md"],
+        "Release Notes": ["CHANGELOG.md"]
+      ]
     ]
   end
 end
